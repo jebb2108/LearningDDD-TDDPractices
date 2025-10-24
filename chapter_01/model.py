1770db95a7aaf020d4902b9ec7a93b8c67827292
jebb <gabouchard2002@gmail.com>
2025-10-24 14:15:21 +0300

    refactored model in prof manner

--- chapter_01/model.py
+++ chapter_01/model.py
@@ -1,15 +1,30 @@
-from datetime import date, timedelta
+from dataclasses import dataclass
+from datetime import date
+from typing import List, Optional
 
-from exc import (
-    SKUDoesntMatchError,
-    NotEnoughQuantityError,
-    AvailableEqualToRequiredError,
-    PrefersWarehouseToShipmentError, PrefersEarlierBatchesError
-)
+
+class OutOfStock(Exception):
+    pass
+
+
+def allocate(line: "OrderLine", batches: List["Batch"]):
+    try:
+        batch = next(b for b in batches if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
 
 
 class Batch:
-    def __init__(self, ref: str, sku: str, qty: int, eta: date):
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
         """
         Класс batch представляет партию товара
         :param ref: reference - артикул товара
@@ -21,25 +36,31 @@ class Batch:
         self.sku = sku
         self.eta = eta
         self.available_quantity = qty
+        self._allocations = set()
 
-    def allocate(self, line: "OrderLine"):
-        """ Проверяет условия заказа """
-        if line.sku != self.sku:
-            raise SKUDoesntMatchError
-        elif line.qty > self.available_quantity:
-            raise NotEnoughQuantityError
-        elif line.qty == self.available_quantity:
-            raise AvailableEqualToRequiredError
-        elif self.eta > date.today():
-            if self.eta >= date.today() + timedelta(days=10):
-                raise PrefersEarlierBatchesError
-            raise PrefersWarehouseToShipmentError
+    def __repr__(self):
+        return f"<Batch: {self.reference}>"
 
-        self.available_quantity -= line.qty
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return self.reference == other.reference
 
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
 
-class OrderLine:
-    def __init__(self, orderid, sku, qty):
-        self.orderid = orderid
-        self.sku = sku
-        self.qty = qty
+    def allocate(self, line: OrderLine):
+        """После проверки заказа, создает заказ"""
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    def can_allocate(self, line: OrderLine):
+        return self.sku == line.sku and self.available_quantity >= line.qty
